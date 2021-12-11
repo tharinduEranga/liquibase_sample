58c45cd05a993ecc2c96362ea95f0f04fd244b04
Tharindu Eranga <95670397+tharinduwiley@users.noreply.github.com>
2021-12-11 18:05:23 +0530

    add sample project files to run and update a liquibase script

---
+++ src/test/java/com/liquidbase/sample/LiquidbaseSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liquidbase.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquidbaseSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
