2ee328a3518a818e7d232eec4f3b20b4e8cbe0c5
Tharindu Eranga <95670397+tharinduwiley@users.noreply.github.com>
2021-12-12 11:54:47 +0530

    add get user test

--- src/test/java/com/liquidbase/sample/LiquidbaseSampleApplicationTests.java
+++ src/test/java/com/liquidbase/sample/LiquidbaseSampleApplicationTests.java
@@ -8,7 +8,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 @SpringBootTest
@@ -29,6 +31,17 @@ class LiquidbaseSampleApplicationTests {
         User user = new User(1, "Tharindu", "0776288969", "Bentota");
         userRepository.add(user);
         verify(userMapper, times(1)).save(user);
+    }
+
+    @Test
+    public void testGetUserById() {
+        when(userMapper.findById(1)).thenReturn(new User(1, "Tharindu", "0776288969", "Bentota"));
+
+        User user = userRepository.findById(1);
+
+        assertEquals("Tharindu", user.getName());
+        assertEquals("0776288969", user.getMobile());
+        assertEquals("Bentota", user.getAddress());
     }
 
 }
